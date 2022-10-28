1f5dbc7f701fd41f8b7ee8da7213fb0e560f26fe
Rupert McKay <rupert.mckay@lokalise.com>
2022-10-28 12:45:51 +0200

    Actually make this project build something

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+  return <span>hello world</span>
+};
+
+render(<App />, document.getElementById("root"));
